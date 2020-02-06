6e460ac382cddbd6de5ab40f66bc40b3b21ff961
bart <bart@localhost.localdomain>
2020-02-06 22:09:56 +0000

    Added singleplayer activity and button

--- Countdown/app/src/main/java/project/thirdYear/countdown/MainActivity.kt
+++ Countdown/app/src/main/java/project/thirdYear/countdown/MainActivity.kt
@@ -1,12 +1,22 @@
 package project.thirdYear.countdown
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val singlePlayerButton : Button = findViewById(R.id.singlePlayerButton)
+
+        singlePlayerButton.setOnClickListener {
+            val intent = Intent(this, SinglePlayerMenu::class.java)
+            startActivity(intent)
+        }
     }
+    
 }
---
+++ Countdown/app/src/main/java/project/thirdYear/countdown/SinglePlayerMenu.kt
@@ -0,0 +1,12 @@
+package project.thirdYear.countdown
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SinglePlayerMenu : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single_player_menu)
+    }
+}
