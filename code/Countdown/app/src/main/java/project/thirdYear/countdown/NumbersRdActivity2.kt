cc673e148533009a7c3ebc9c61df4632dce9d439
bart <bart@localhost.localdomain>
2020-02-24 16:04:23 +0000

    Kotlin version of number round solver

--- code/Countdown/app/src/main/java/project/thirdYear/countdown/NumbersRdActivity2.kt
+++ code/Countdown/app/src/main/java/project/thirdYear/countdown/NumbersRdActivity2.kt
@@ -1,12 +1,20 @@
 package project.thirdYear.countdown
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_numbers_rd2.*
 
 class NumbersRdActivity2 : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_numbers_rd2)
+
+        solve.setOnClickListener {
+            var solver: NumsSolver = NumsSolver()
+            var nums = arrayListOf(75,8,6,10,25,7)
+            var answer = solver.solve(nums, 785)
+            answerText.text = "abc"
+        }
     }
 }
--- code/Countdown/app/src/main/java/project/thirdYear/countdown/NumsSolver.kt
+++ code/Countdown/app/src/main/java/project/thirdYear/countdown/NumsSolver.kt
@@ -11,30 +11,58 @@ class NumsSolver {
         return stack
     }
 
-    fun solve(nums:ArrayList<Int>, target:Int) {
+    fun permutation(numList:ArrayList<Int>):ArrayList<ArrayList<Int>> {
+        var perms = ArrayList<ArrayList<Int>>()
+        perms.add(ArrayList<Int>())
+        for (i in 0 until numList.size)
+        {
+            var current = ArrayList<ArrayList<Int>>()
+            for (l in perms)
+            {
+                for (j in 0 until l.size + 1)
+                {
+                    l.add(j, numList.get(i))
+                    var temp = ArrayList<Int>(l)
+                    current.add(temp)
+                    l.removeAt(j)
+                }
+            }
+            perms = ArrayList<ArrayList<Int>>(current)
+        }
+        return perms
+    }
 
-        var operators = mapOf<String,(Int,Int)->Int>(
-                "+" to {x:Int, y:Int -> x + y},
-                "-" to {x:Int, y:Int -> x - y},
-                "*" to {x:Int, y:Int -> x * y},
-                "/" to {x:Int, y:Int -> x / y})
 
-        var second: ArrayList<String> = next_op(nums.get(1))
-        var third: ArrayList<String> = next_op(nums.get(2))
-        var fourth: ArrayList<String> = next_op(nums.get(3))
-        var fifth: ArrayList<String> = next_op(nums.get(4))
-        var sixth: ArrayList<String> = next_op(nums.get(5))
 
-        var num: Int = nums.get(0)
+    fun solve(nums:ArrayList<Int>, target:Int): String {
 
-        for (s in second){
-            for (t in third){
-                for (o in fourth){
-                    for (i in fifth){
-                        for (x in sixth){
+        var operators = mapOf<String, (Int, Int) -> Int>(
+                "+" to { x: Int, y: Int -> x + y },
+                "-" to { x: Int, y: Int -> x - y },
+                "*" to { x: Int, y: Int -> x * y },
+                "/" to { x: Int, y: Int -> x / y })
+
+        var listOfLists = permutation(nums)
+
+        for (list in listOfLists) {
+
+            var second: ArrayList<String> = next_op(list.get(1))
+            var third: ArrayList<String> = next_op(list.get(2))
+            var fourth: ArrayList<String> = next_op(list.get(3))
+            var fifth: ArrayList<String> = next_op(list.get(4))
+            var sixth: ArrayList<String> = next_op(list.get(5))
+
+            var num: Int = list.get(0)
+
+            for (s in second) {
+                for (t in third) {
+                    for (o in fourth) {
+                        for (i in fifth) {
+                            for (x in sixth) {
+
+                            }
 
                         }
-
                     }
                 }
             }
